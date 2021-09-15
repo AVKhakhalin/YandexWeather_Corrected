82ee11f41158327f35cf6ef9486e1d0d7dcb4797
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-09-15 16:41:53 +0300

    Initial commit Восстановление проекта после ошибки. Проёденные уроки 2-4.

---
+++ app/src/main/java/ru/geekbrains/lessions2345/yandexweather_corrected/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.geekbrains.lessions2345.yandexweather_corrected
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
